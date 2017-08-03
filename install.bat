2c825c989f851c57f5d2284b0840379c8bcd3cb8
Mitesh <patelmitesha@gmail.com>
2017-08-03 12:34:49 +0530

    Installation procedure modified

--- install.bat
+++ install.bat
@@ -1,17 +1,18 @@
 echo "Started building your web development environment"
 
-CD I:
+c:
+CD C:\Users\mitesh\Downloads
 set PATH=%PATH%;C:\Program Files\7-Zip
 
-powershell -command "& { (New-Object Net.WebClient).DownloadFile('https://nodejs.org/dist/v8.2.1/node-v8.2.1-win-x64.zip', 'i:\node.zip') }"
+powershell -command "& { (New-Object Net.WebClient).DownloadFile('https://nodejs.org/dist/v8.2.1/node-v8.2.1-win-x64.zip', 'C:\Users\mitesh\Downloads\node.zip') }"
 
-7z x i:\node.zip -oI:\node
+7z x C:\Users\mitesh\Downloads\node.zip -oC:\Users\mitesh\Downloads\node
 
-set PATH=%PATH%;I:\node\node-v8.2.1-win-x64
+set PATH=%PATH%;C:\Users\mitesh\Downloads\node\node-v8.2.1-win-x64
 
-powershell -command "& { (New-Object Net.WebClient).DownloadFile('https://download.sublimetext.com/Sublime Text Build 3126 x64.zip', 'i:\sublime.zip') }"
+powershell -command "& { (New-Object Net.WebClient).DownloadFile('https://download.sublimetext.com/Sublime Text Build 3126 x64.zip', 'C:\Users\mitesh\Downloads\sublime.zip') }"
 
 
-7z x i:\sublime.zip -oI:\sublime
+7z x C:\Users\mitesh\Downloads\sublime.zip -oC:\Users\mitesh\Downloads\sublime
 
 
